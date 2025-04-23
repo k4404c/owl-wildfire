3cb560df732089272ceb924937dc3cf5089f091d
Sai Shiva Satwik Mallajosyula <satwikug@gmail.com>
2025-04-22 19:50:43 -0700

    url change

--- dashboard2/backend/server.js
+++ dashboard2/backend/server.js
@@ -10,7 +10,7 @@ app.use(cors({
   origin: [
     'http://localhost:5173',
     'http://localhost:3001',
-    'https://owl-wildfire.vercel.app'  // Remove trailing slash
+    'https://owl-wildfire1.vercel.app'  // Remove trailing slash
   ],
   methods: ['GET', 'POST'],
   credentials: true
